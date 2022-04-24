9ca44f8bcee587134f5fc193c52de5688a208580
awais512 <raza.awais25@gmail.com>
2022-04-24 05:16:24 +0500

    Aws Amplify setup and Authentication

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+
 import RootNavigator from './src/navigation';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { Amplify } from 'aws-amplify';
+import config from './src/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default function App() {
+Amplify.configure(config);
+
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
@@ -13,3 +19,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
